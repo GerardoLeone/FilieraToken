7034c1f9c3c01111b30188fceb3929db0ae7e17f
Umberto Della Monica <umberto.dellamonica@hotmail.it>
2024-02-04 18:24:08 +0100

    add Smart Contract Consumer

---
+++ Consumer-smart-contract/ConsumerService.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ConsumerStorage.sol";
+import "./Filieratoken.sol";
+
+
+contract ConsumerService {
+
+    // Address of Consumer Storage 
+    ConsumerStorage private consumerStorage;
+    // Address of Token FT - ERC-20
+    Filieratoken private filieraToken;
+
+    // Address of Organization che gestisce gli utenti
+    address private  ConsumerOrg;
+
+    // Evento emesso al momento della cancellazione di un consumatore
+    event ConsumerDeleted(address indexed walletConsumer, string message);
+
+    // Evento emesso al momento della registrazione di un consumatore
+    event ConsumerRegistered(address indexed walletConsumer, string fullName, string message);
+
+    // We must deploy first : 
+    // - ConsumerStorage 
+    // - ConsumerService 
+    constructor(address _consumerStorage, address _filieraToken) {
+        consumerStorage = ConsumerStorage(_consumerStorage);
+        filieraToken = Filieratoken(_filieraToken);
+        ConsumerOrg = msg.sender;
+    }
+
+    // Function to Register Consumer to Application 
+    // Settiamo  - msg.sender come il valore del wallet del Consumer 
+    function registerConsumer(string memory fullName, string memory password, string memory email) external {
+        // Verifico che l'address sia diverso da quello di Deploy 
+        require(msg.sender != ConsumerOrg,"Address non Valido!");
+        //Call function of Storage 
+        consumerStorage.addUser(fullName, password, email, msg.sender);
+
+        filieraToken.transfer(msg.sender, 100);
+
+        // Emit Event on FireFly 
+        emit ConsumerRegistered(msg.sender, fullName, "Utente e' stato registrato!");
+    }
+
+    function getConsumerData() external view returns (uint256, string memory, string memory, string memory, uint256) {
+        // Call function of Storage 
+        address walletConsumer = msg.sender;
+        // Verifico che l'address sia diverso da quello di Deploy 
+        require(walletConsumer != ConsumerOrg,"Address non Valido!");
+
+        return consumerStorage.getUser(walletConsumer);
+    }
+
+
+
+    // Delete Consumer - we can delete consumer on Blockchain
+    function deleteConsumer() external {
+        address walletConsumer = msg.sender;
+        require(consumerStorage.deleteUser(walletConsumer), "Errore durante la cancellazione");
+        // Burn all token 
+        filieraToken.burnToken(walletConsumer, filieraToken.balanceOf(walletConsumer));
+        // Emit Event on FireFly 
+        emit ConsumerDeleted(walletConsumer,"Utente e' stato eliminato!");
+    }
+
+}
---
+++ Filiera-Token-SmartContract/contracts/Filieratoken.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Filieratoken is ERC20 {
+
+  // Nome del token
+  string private constant _name = "FilieraToken";
+
+  // Simbolo del token
+  string private constant _symbol = "FLT";
+
+  // Fornitura totale di token
+  uint256 private constant _totalSupply = 1000000000; // Modifica questo valore in base alle tue esigenze
+
+  constructor() ERC20(_name, _symbol) {
+    // Assegna la fornitura totale al deployer
+    _mint(address(this), _totalSupply);
+  }
+
+  // Funzione per visualizzare il saldo di un utente
+  function balanceOf(address account) public view override returns (uint256) {
+    return super.balanceOf(account);
+  }
+
+  // Funzione per trasferire token da un utente all'altro
+  function transfer(address to, uint256 amount) public  override returns (bool) {
+     super.transferFrom(address(this), to, amount);
+     return true;
+  }
+
+
+  function burnToken(address user, uint256 balance) public  returns (bool) {
+    super._burn(user, balance);
+    return true;
+  }
+
+  // Funzione per approvare un altro spender a trasferire token per tuo conto
+  function approve(address spender, uint256 amount) public override returns (bool) {
+    return super.approve(spender, amount);
+  }
+
+}

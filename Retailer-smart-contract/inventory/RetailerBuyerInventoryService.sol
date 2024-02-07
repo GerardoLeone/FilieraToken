d31e51b8c0563459d1050d5b64182c3576239400
Umberto Della Monica <umberto.dellamonica@hotmail.it>
2024-02-07 16:41:52 +0100

    update smart contract
    Update smart contract

--- Retailer-smart-contract/inventory/RetailerBuyerInventoryService.sol
+++ Retailer-smart-contract/inventory/RetailerBuyerInventoryService.sol
@@ -2,6 +2,12 @@
 pragma solidity ^0.8.21;
 
 contract RetailerBuyerInventoryService {
+    
+    // Address of Organization che gestisce gli utenti
+    address private  RetailerOrg;
 
+    constructor(){
+        RetailerOrg = msg.sender;
+    }
     
 }
